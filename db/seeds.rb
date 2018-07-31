23ae2a05e94c7e9354cefdf3482e87c5faf8bc3a
Gus Macfarlane <gusmac@gmail.com>
2018-07-31 15:36:43 +0800

    pushing to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'json'
-# require 'open-uri'
+require 'json'
+require 'open-uri'
 
-# response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
-# json = JSON.parse(response.read)
+response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+json = JSON.parse(response.read)
 
-# json["drinks"].each do |item|
-#   Ingredient.create(name: item["strIngredient1"])
-# end
-
-require 'faker'
-
-20.times do
-  Cocktail.create(name: Faker::Beer.name)
+json["drinks"].each do |item|
+  Ingredient.create(name: item["strIngredient1"])
 end
+
+# require 'faker'
+
+# 20.times do
+#   Cocktail.create(name: Faker::Beer.name)
+# end
